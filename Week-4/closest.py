2edb0ab0a66be20da0ddc362eb87cf886c77787d
Abhishek <AbhiSaphire@github.com>
2020-06-14 00:58:05 +0530

    Week4 / Solution 6 Optimized

--- Week-4/closest.py
+++ Week-4/closest.py
@@ -1,90 +1,60 @@
 # python3
 # https://medium.com/@andriylazorenko/closest-pair-of-points-in-python-79e2409fc0b2 
 
-import math
-def dist(p1, p2):
-    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+# ~~OPTIMIZED SOLUTION~~
+from math import sqrt
+from itertools import combinations
 
-def closest_split_pair(p_x, p_y, delta, best_pair):
-    ln_x = len(p_x)  # store length - quicker
-    mx_x = p_x[ln_x // 2][0]  # select midpoint on x-sorted array
+# store best solution here
+Best = None
 
-    # Create a subarray of points not further than delta from midpoint on x-sorted array
-    s_y = [x for x in p_y if mx_x - delta <= x[0] <= mx_x + delta]
+def calc_distance(p1, p2):
+    global Best
+    dx = p1[0]-p2[0]
+    dy = p1[1]-p2[1]
+    if dx < Best[2] and dy < Best[2]:
+        dist = sqrt(dx**2 + dy**2)
+        if dist < Best[2]:
+            Best = p1, p2, dist
 
-    best = delta  # assign delta value to best
-    ln_y = len(s_y)  # store length of subarray for quickness
-    for i in range(ln_y - 1):
-        for j in range(i+1, min(i + 5, ln_y)):    # We have to check only next 5 points; proof found in literature
-            p, q = s_y[i], s_y[j]
-            dst = dist(p, q)
-            if dst < best:
-                best_pair = p, q
-                best = dst
-    return best_pair[0], best_pair[1], best
+def brute_force(points):
+    for p1, p2 in combinations(points, 2):
+        calc_distance(p1, p2)
 
-def brute(ax):
-    mi = dist(ax[0], ax[1])
-    p1 = ax[0]
-    p2 = ax[1]
-    ln_ax = len(ax)
-    if ln_ax == 2:
-        return p1, p2, mi
-    for i in range(ln_ax-1):
-        for j in range(i + 1, ln_ax):
-            if i != 0 and j != 1:
-                d = dist(ax[i], ax[j])
-                if d < mi:  # Update min_dist and points
-                    mi = d
-                    p1, p2 = ax[i], ax[j]
-    return p1, p2, mi
+def closest_pair(points):
+    global Best
+    Best = [None, None, float("inf")]
+    x_sorted = sorted(points)
+    find_closest(x_sorted)
+    print(Best)
+    return Best[:2]
 
-def closest_pair(ax, ay):
-    ln_ax = len(ax)  # It's quicker to assign variable
-    if ln_ax <= 3:
-        return brute(ax)  # A call to bruteforce comparison
-    mid = ln_ax // 2  # Division without remainder, need int
-    Qx = ax[:mid]  # Two-part split
-    Rx = ax[mid:]
+def find_closest(x_sorted):
+    # perform brute force for 3 or 2 points
+    points_count = len(x_sorted)
+    if points_count <= 3:
+        return brute_force(x_sorted)
+    # split into two halves (x line) -> keep sorted by x and y
+    half = points_count // 2
+    _x_sorted1, _x_sorted2 = x_sorted[:half], x_sorted[half:]
+    find_closest(_x_sorted1)
+    find_closest(_x_sorted2)
+    # check best result of split pair
+    find_closest_split_pair(x_sorted)
 
-    midpoint = ax[mid][0]
-    Qy = list()
-    Ry = list()
-    for x in ay:  # split ay into 2 arrays using midpoint
-        if x[0] < midpoint:
-           Qy.append(x)
-        else:
-           Ry.append(x)
-    # Call recursively both arrays after split
-    (p1, q1, mi1) = closest_pair(Qx, Qy)
-    (p2, q2, mi2) = closest_pair(Rx, Ry)
+def find_closest_split_pair(x_sorted):
+    points_count = len(x_sorted)
+    mid_x = x_sorted[points_count // 2][0]
+    # consider only point that are no further than best distance from x
+    remained_points = sorted([p for p in x_sorted if mid_x - Best[2] < p[0] < mid_x + Best[2]], key=lambda p: p[1])
+    remained_count = len(remained_points)
+    for i, p1 in enumerate(remained_points[:-1]):
+        for p2 in remained_points[i+1:min(i+7, remained_count)]:
+            calc_distance(p1, p2)
 
-    # Determine smaller distance between points of 2 arrays
-    if mi1 <= mi2:
-        d = mi1
-        mn = (p1, q1)
-    else:
-        d = mi2
-        mn = (p2, q2)
-
-    # Call function to account for points on the boundary
-    (p3, q3, mi3) = closest_split_pair(ax, ay, d, mn)
-    # Determine smallest distance for the array
-    if d <= mi3:
-        return mn[0], mn[1], d
-    else:
-        return p3, q3, mi3
-
-def solution(a):
-    ax = sorted(a, key=lambda x: x[0])  # Presorting x-wise O(nlogn)
-    ay = sorted(a, key=lambda x: (x[1], x[0]))  # Presorting y-wise then x-wise O(nlogn)
-    p1, p2, mi = closest_pair(ax, ay)  # Recursive D&C function
-    return mi
-
-# Input
-points = list()
 n = int(input())
-for i in range(n):
-    points.append([int(i) for i in input().split()])
-
-print(solution(points))
+points = []
+for _ in range(n):
+	point = [int(x) for x in input().split()]
+	points.append(point)
+closest_pair(points)
